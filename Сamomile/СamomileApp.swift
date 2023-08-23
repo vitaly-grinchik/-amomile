37003596e6c13b5d5220db7e9e1ded0b5d84b211
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-08-23 16:22:52 +0300

    Initial Commit

---
+++ Сamomile/СamomileApp.swift
@@ -0,0 +1,17 @@
+//
+//  СamomileApp.swift
+//  Сamomile
+//
+//  Created by Виталий Гринчик on 23.08.23.
+//
+
+import SwiftUI
+
+@main
+struct СamomileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PetalView()
+        }
+    }
+}
